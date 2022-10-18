34718e93a9984840f8a8c4ad8926dd2c59d9ef9a
Bishal Aryal <bishalaryal1067@gmail.com>
2022-10-18 14:39:38 +0545

    search page

--- search.php
+++ search.php
@@ -9,26 +9,43 @@
 </head>
 
 <style>
-    .result-container{
+    @import url('https://fonts.googleapis.com/css2?family=Lexend&display=swap');
+
+    * {
+        font-family: 'Lexend', sans-serif;
+    }
+
+    h1{
+        text-align: center;
+        margin-top: 2.5em;
+    }
+
+    .result-container {
+        margin: 2.5em auto;
+        padding: 1.5em 3em;
         width: 90%;
         height: auto;
         display: grid;
         grid-template-columns: repeat(auto-fit, minmax(30%, 100%));
-        grid-template-rows: repeat(auto-fit, minmax(30%,30%));
+        grid-template-rows: repeat(auto-fit, minmax(30%, 30%));
         column-gap: .75em;
         row-gap: .5em;
     }
 
-    .video-results, .news-results, .fixture-results{
+    .video-results,
+    .news-results,
+    .fixture-results {
         width: 100%;
         display: grid;
         grid-template-columns: repeat(auto-fit, minmax(25%, 25%));
-        grid-template-rows: repeat(auto-fit, minmax(25em,25em));
+        grid-template-rows: repeat(auto-fit, minmax(25em, 25em));
         column-gap: .75em;
         row-gap: .5em;
     }
 </style>
+
 <body>
+    <?php include('header.php') ?>
     <h1>Search results for '$search_query':</h1>
     <div class="result-container">
         <div class="video-results"></div>
@@ -36,4 +53,5 @@
         <div class="fixture-results"></div>
     </div>
 </body>
+
 </html>
